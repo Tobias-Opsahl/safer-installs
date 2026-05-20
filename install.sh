d75f252608029810696548d52605d916f3abb782
Tobias Aanderaa Opsahl <tobias.aanderaa.opsahl@dnb.no>
2026-05-20 18:11:52 +0200

    Add safer npm setup
    Add configuration to pnpm, aliases so that npm and npx and variants can
    not be run, add the safer-npx, which uses pnpm add with the
    configurations to install into a new directory, run it, and clean up,
    and add a script for installing (copying over the files).

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+# Paths can be changed
+CONFIG_PATH="$HOME/.config"
+BIN_PATH="$HOME/.local/bin/user-scripts"
+
+DISPLAY_PATH="${CONFIG_PATH/$HOME/\$HOME}"
+
+YELLOW=$'\033[1;33m'
+RESET=$'\033[0m'
+PREFIX="${YELLOW}[safer-installs]${RESET} "
+
+run() {
+    echo "    $*"
+    "$@"
+}
+
+echo "${PREFIX}Setting up safer-installs by copying over files..."
+
+run mkdir -p "$CONFIG_PATH/safer-installs"
+run cp .zshrc-node .zshrc-python "$CONFIG_PATH/safer-installs"
+run mkdir -p "$CONFIG_PATH/pnpm"
+run cp dotfiles/config.yaml "$CONFIG_PATH/pnpm/config.yaml"
+run mkdir -p "$BIN_PATH"
+run cp safer-npx "$BIN_PATH"
+
+echo "${PREFIX}Add the following lines to ~/.zshrc to finish setting up:"
+echo "source \"$DISPLAY_PATH/safer-installs/.zshrc-node\""
+echo "source \"$DISPLAY_PATH/safer-installs/.zshrc-python\""
